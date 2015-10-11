db1fc39cc1d742823961a9b0ae2c94445bc52be0
Bert Outtier <outtierbert@gmail.com>
2015-10-11 14:17:08 +0000

    added pygame screen blit

--- display.py
+++ display.py
@@ -1,18 +1,27 @@
 import pygame
+import io
+from PIL import Image
 
 class Display:
     def __init__(self):
         self.open()
 
-    def show_image(self, image):
-        image = self.scale_image(image)
-        self.screen.blit(image, (0, 0))
+    def show_product(self, image, brand, description):
+        img = Image.open(io.BytesIO(image))
+        gameimg = pygame.image.fromstring(img.tobytes(), img.size, "RGB")
+	gameimgpos = gameimg.get_rect(centerx=self.screen.get_width()/2, centery=self.screen.get_height()/2)
+        self.screen.blit(gameimg, gameimgpos)
+
+	font = pygame.font.Font(None, 36)
+	text = font.render("%s - %s" % (brand, description), 1, (255, 255, 255))
+	textpos = text.get_rect(midtop=(self.screen.get_width()/2, (self.screen.get_height()/2 + gameimg.get_height()/2 + 10)))
+	textpos.centerx = self.screen.get_rect().centerx
+	self.screen.blit(text, textpos)
+
         pygame.display.flip()
 
-    def scale_image(self, image):
-        return pygame.transform.scale(pygame.image.load(image), self.size)
-
     def open(self):
+        pygame.init()
         pygame.display.init()
         pygame.mouse.set_visible(False)
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
