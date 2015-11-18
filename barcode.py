6149365ecf504c5c955823be8838775a1613be6a
Bert Outtier <outtierbert@gmail.com>
2015-11-18 21:41:37 +0100

    fix scanning

--- barcode.py
+++ barcode.py
@@ -17,17 +17,54 @@ class BarcodeScanner(Thread):
 	def terminate(self):
 		self.input.close()
 		self.quit = True
-		
+
 	def scan(self):
-	    try:
-	        line = self.input.readline()
-	    except KeyboardInterrupt:
-	    	self.quit = True
-	    if not line:
-	    	self.quit = True
-	    print line
-	    self.callback(line)
-	    self.quit = True
+		barcode = ''
 
+		continue_looping = True
 
-		
+		while continue_looping:
+		    report = self.input.read(8)
+		    for i in report:
+		        j = ord(i)
+		        if j == 0:
+		            continue
+
+		        if j == 0x1E:
+		            barcode += '1'
+		            continue
+		        elif j == 0x1F:
+		            barcode += '2'
+		            continue
+		        elif j == 0x20:
+		            barcode += '3'
+		            continue
+		        elif j == 0x21:
+		            barcode += '4'
+		            continue
+		        elif j == 0x22:
+		            barcode += '5'
+		            continue
+		        elif j == 0x23:
+		            barcode += '6'
+		            continue
+		        elif j == 0x24:
+		            barcode += '7'
+		            continue
+		        elif j == 0x25:
+		            barcode += '8'
+		            continue
+		        elif j == 0x26:
+		            barcode += '9'
+		            continue
+		        elif j == 0x27:
+		            barcode += '0'
+		            continue
+		        elif j == 0x28:
+		            continue_looping = False
+		            break
+		        else:
+		            pass
+		self.callback(barcode)
+		self.quit = True
+			
