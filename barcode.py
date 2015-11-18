407d2d3ad5f710e9ee7cd54cef6c5da4a1cef2c9
Bert Outtier <outtierbert@gmail.com>
2015-11-18 21:22:13 +0100

    fix input

--- barcode.py
+++ barcode.py
@@ -1,9 +1,8 @@
-import io
 from threading import Thread
-import sys
 
 class BarcodeScanner(Thread):
-	def __init__(self, callback=None):
+	def __init__(self, callback=None, device='/dev/hidraw0'):
+		self.input = open(device, 'rb')
 		self.callback = callback
 		self.quit = False
 		Thread.__init__(self)
@@ -16,16 +15,17 @@ class BarcodeScanner(Thread):
 		self.scan()
 
 	def terminate(self):
+		self.input.close()
 		self.quit = True
 		
 	def scan(self):
 	    try:
-	        line = sys.stdin.readline()
+	        line = self.input.readline()
 	    except KeyboardInterrupt:
 	    	self.quit = True
 	    if not line:
 	    	self.quit = True
-
+	    print line
 	    self.callback(line)
 	    self.quit = True
 
